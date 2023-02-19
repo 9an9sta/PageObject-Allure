7add7fd274223ddccec852f9478ec6eb8085907f
yenzyne <100135673+Yenzyne@users.noreply.github.com>
2023-02-19 12:27:37 +0200

    add @Step in Brand page

--- src/main/java/framework/pages/BrandsPage.java
+++ src/main/java/framework/pages/BrandsPage.java
@@ -1,5 +1,6 @@
 package framework.pages;
 
+import io.qameta.allure.Step;
 import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -8,6 +9,8 @@ import java.util.List;
 @Log4j2
 public class BrandsPage extends BasePage{
     private By allBrands = By.xpath("//div[@class='col-sm-3']/a");
+
+    @Step("Find all brand in [Brand] page")
 
     public List<WebElement> findAllBrands(){
         log.info("Find all Brands in page");
